22c0ec287d164b917da540b7856bd9c1f9f20556
Christian Nightingale <cjn0019@auburn.edu>
2020-06-04 20:44:35 -0700

    correct print layout

--- gallery/tools/db.py
+++ gallery/tools/db.py
@@ -37,23 +37,16 @@ def list_users():
     print()
         
 def add_user(username,password,full_name):
-    print("add_user")
     execute("INSERT INTO users (username, password, full_name) VALUES (%s,%s,%s);",(username,password,full_name))
-    print()
     
 def edit_user(username,password,full_name):
-    print("edit_user")
     execute("UPDATE users SET password=%s,full_name=%s WHERE username=%s;",(password,full_name,username))
-    print()
     
 def delete_user(username):
-    print("delete_user")
     execute("DELETE FROM users WHERE username=%s;",(username,))
-    print()
     
 def quit_menu():
     print("quit_menu")
-    print()
     
 def main():
     connect()
@@ -69,24 +62,29 @@ def main():
         print()
         if command == "1":
             list_users()
+            print()
         elif command == "2":
             username = input("Username> ")
             password = input("Password> ")
             full_name = input("Full name> ")
             add_user(username,password,full_name)
+            print()
         elif command == "3":
             username = input("Username to edit> ")
             password = input("New password (press enter to keep current)> ")
             full_name = input("full name (press enter to keep current)> ")
             edit_user(username,password,full_name)
+            print()
         elif command == "4":
             username = input("enter username to delete> ")
             answer = input("Are you sure that you want to delete "+username+"?  y/n: ") 
             if answer.lower() == "y":
                 print("Deleted.")
                 delete_user(username)
+            print()
         elif command == "5":
             quit_menu()
+            print()
             break
         else: 
             print("Invalid command entered, try again; if you dare.")
