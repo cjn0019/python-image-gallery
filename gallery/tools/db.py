36c29ee4825831b55971ad983dba5a32dd30a770
Christian Nightingale <cjn0019@auburn.edu>
2020-06-10 00:07:07 -0700

    added get_dbname as variable

--- gallery/tools/db.py
+++ gallery/tools/db.py
@@ -2,8 +2,6 @@ import psycopg2
 import json
 from secrets import get_secret_image_gallery
 
-db_name = "image_gallery"
-
 connection = None
 
 def get_secret():
@@ -19,10 +17,13 @@ def get_host(secret):
 def get_username(secret):
     return secret['username']
 
+def get_dbname(secret):
+    return secret['database_name']
+
 def connect():
     global connection
     secret = get_secret()
-    connection = psycopg2.connect(host=get_host(secret), dbname=db_name, user=get_username(secret), password=get_password(secret))
+    connection = psycopg2.connect(host=get_host(secret), dbname=get_dbname(secret), user=get_username(secret), password=get_password(secret))
 
 def execute(query,args=None):
     global connection
