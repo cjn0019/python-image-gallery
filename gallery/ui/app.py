af1d4733110291bd25766e5ff1f2e04734e9a79e
Christian Nightingale <cjn0019@auburn.edu>
2020-06-11 19:46:43 -0700

    added app.py

---
+++ gallery/ui/app.py
@@ -0,0 +1,21 @@
+from flask import flask
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return """
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Christian Nightingale's Auburn Python Web App</title>
+        <meta charset="utf-8" />
+    </head>
+    <body>
+        <h1>Christian Nightingale's Auburn Python Web App</h1>
+    </body>
+</html>
+"""
+
+@app.route('/goodbye')
+def goodbye():
+    return 'Goodbye'
