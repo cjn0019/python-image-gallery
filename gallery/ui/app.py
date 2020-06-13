88169990edb004fc2bd06672de13047fb2742e8e
Christian Nightingale <cjn0019@auburn.edu>
2020-06-12 19:25:30 -0700

    moving code up

--- gallery/ui/app.py
+++ gallery/ui/app.py
@@ -2,46 +2,6 @@ from flask import Flask
 from flask import request
 from flask import render_template
 
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return """
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Christian Nightingale's Auburn Python Web App</title>
-        <meta charset="utf-8" />
-    </head>
-    <body>
-        <h1>Christian Nightingale's Auburn Python Web App</h1>
-    </body>
-</html>
-"""
-
-@app.route('/admin')
-def admin():
-    list_users()
-    return 'Goodbye'
-
-@app.route('/greet/<name>')
-def greet(name):
-    return 'Nice to meet you ' + name
-
-@app.route('/add/<int:x>/<int:y>', methods = ['GET'])
-def add(x, y):
-    return 'The sum is ' + str(x + y)
-
-@app.route('/mult')
-def mult():
-    x = request.args['x']
-    y = request.args['y']
-    return 'The product is ' + str(int(x)*int(y))
-
-@app.route('/calculator')
-def calculator():
-    return render_template('calculator.html')
-
 import psycopg2
 import json
 from secrets import get_secret_image_gallery
@@ -115,3 +75,45 @@ def delete_user(username):
     else:
         execute("DELETE FROM users WHERE username=%s;",(username,))
     
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return """
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Christian Nightingale's Auburn Python Web App</title>
+        <meta charset="utf-8" />
+    </head>
+    <body>
+        <h1>Christian Nightingale's Auburn Python Web App</h1>
+    </body>
+</html>
+"""
+
+@app.route('/admin')
+def admin():
+    list_users()
+    return 'Goodbye'
+
+@app.route('/greet/<name>')
+def greet(name):
+    return 'Nice to meet you ' + name
+
+@app.route('/add/<int:x>/<int:y>', methods = ['GET'])
+def add(x, y):
+    return 'The sum is ' + str(x + y)
+
+@app.route('/mult')
+def mult():
+    x = request.args['x']
+    y = request.args['y']
+    return 'The product is ' + str(int(x)*int(y))
+
+@app.route('/calculator')
+def calculator():
+    return render_template('calculator.html')
+
